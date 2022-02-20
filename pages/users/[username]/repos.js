e89aec17c2e25cd3627eef73a9e8d4c8af85f615
jianan1104 <linjianan1104@gmail.com>
2022-02-21 00:57:28 +0800

    Fix onReachBottom bugs.

--- pages/users/[username]/repos.js
+++ pages/users/[username]/repos.js
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { React, useState, useCallback } from 'react';
 import api from '../../../modules/api';
 import RepositoryList from "../../../components/RepositoryList";
 import MenuComponent from '../../../components/Menu';
