05e929319be0883b1bfd2dfaaa58fd12997777fc
jianan1104 <linjianan1104@gmail.com>
2022-03-10 20:55:04 +0800

    Add ErrorBoundary page.

--- pages/_error.js
+++ pages/_error.js
@@ -1,13 +1,25 @@
-import { Image } from 'semantic-ui-react';
+import { Image, Button, Modal } from 'semantic-ui-react';
+import { useRouter } from 'next/router'
 
 function Error({ statusCode }) {
+    const router = useRouter();
+    const home = () => {
+        router.push('/');
+    }
     return (
         <>
         <div style={{ display: 'flex' , flexDirection: 'column',alignItems: 'center', justifyContent: 'center', height: '100vh'}}>
             <Image src="https://i.imgur.com/c738l7Q.jpg" size='large'/>
             <p>
+                房間代碼: { statusCode } <br />
                 痾...你好像走錯房間，被你看到我們在打UNO了，一起玩？ 
             </p>
+            <Modal
+                trigger={<Button primary>一起玩！！</Button>}
+                header='一起玩！'
+                content='要一起玩要先找我來面試，到時我會帶一副UNO.. XD'
+                actions={[{ key: 'cancel', content: '算了', color: 'red', onClick: home }, { key: 'done', content: '好啊', positive: true, onClick: home }]}
+                />
         </div>
         </>
     )
